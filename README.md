6b16f31c571535460e1ef0e34f56f08aae34d853
bojanpotocnik <info@bojanpotocnik.com>
2019-05-13 10:28:54 +0200

    Rewrite telnet data reading

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 
 > [SEGGER's Real Time Transfer (RTT)](https://www.segger.com/products/debug-probes/j-link/technology/about-real-time-transfer/) is the new technology for interactive user I/O in embedded applications. It combines the advantages of SWO and semihosting at very high performance.
 
-SEGGER already provides [J-Link RTT Viewer](https://www.segger.com/products/debug-probes/j-link/technology/about-real-time-transfer/#j-link-rtt-viewer) tool for viewing the RTT output, however despite being practical the tool has limited scrollback and its _Scroll to the end_ cannot be paused.
-Theferore if one wish to look back at some output line, the first problem is that this line may already be out of the 100-lines scrollback limit, and even it is not, every new received message will make the terminal jump to the end.
+SEGGER already provides [J-Link RTT Viewer](https://www.segger.com/products/debug-probes/j-link/technology/about-real-time-transfer/#j-link-rtt-viewer) tool for viewing the RTT output, however despite being practical the tool has limited scrollback, its _Scroll to the end_ cannot be paused and it misses some output when _a lot_ of debug is printed using the highest interface speeds.
+Therefore if one wish to look back at some output line, the first problem is that this line may already be out of the 100-lines scrollback limit, and even it is not, every new received message will make the terminal jump to the end.
 
 This script outputs the data to the standard console output, which can have any scrollback set and can be (un)paused with right-clicking.
 
-**Note**: Above mentioned _J-Link RTT Viewer_ **must be running** for this script to work (output will not be visible in _RTT Viewer_ as it is automatically redirected when any telnet client connects). One can then use tool such as [RBTray](http://rbtray.sourceforge.net/) to "minimize" the _RTT Viewer_ window to the system tray.
+**Note**: J-Link driver/server **must be running** for this script to work. This can be done by:
+ - using _J-Link Commander_ (preferred, more lightweight) with command `JLink -Device <DEVICE> -If <IF> -AutoConnect 1 -Speed <kHz>` (e.g. `JLink.exe -Device NRF52840_xxAA -AutoConnect 1 -If SWD -Speed 50000`)
+ - running the above mentioned _J-Link RTT Viewer_ (output will not be visible in _RTT Viewer_ as it is automatically redirected when any telnet client connects)
 
-Branch [pylink](/bojanpotocnik/segger-rtt-viewer/tree/pylink) removes this requirement with the cost of lower performance.
+One can then use tool such as [RBTray](http://rbtray.sourceforge.net/) to "minimize" the _J-Link Commander_/_RTT Viewer_ window to the system tray.
+
+Branch [pylink](https://github.com/bojanpotocnik/segger-rtt-viewer/tree/pylink) removes this requirement with the cost of lower performance.
 
 <br>
 
--- main.py
+++ main.py
@@ -1,37 +1,77 @@
+import sys
+import time
+import typing
+
 __author__ = "Bojan Potočnik"
 
 import ctypes
 import os
 import telnetlib
-from collections.abc import Iterator
-from typing import Union, Type
 
 
-class SeggerRTTListener(Iterator):
-    def __init__(self):
-        self.telnet = telnetlib.Telnet()  # type: telnetlib.Telnet
+class SeggerRTTListener:
+    def __init__(self, host: str = "localhost", port: int = 19021):
+        self.telnet = telnetlib.Telnet(timeout=1)  # type: telnetlib.Telnet
+        self.host = host
+        self.port = port
 
     def __enter__(self):
-        self.telnet.open("127.0.0.1", 19021)
-        print(f"SeggerRTTListener connected to {self.telnet.host}:{self.telnet.port}.")
+        try:
+            self.telnet.open(self.host, self.port)
+        except ConnectionRefusedError:
+            raise ConnectionRefusedError(
+                f"Could not connect to {self.host}:{self.port}."
+                f" Are you sure that the JLink is running?"
+                f" You can run it with 'JLink -Device <DEVICE> -If <IF> -AutoConnect 1 -Speed <kHz>'"
+                f", e.g. 'JLink --Device NRF52840_xxAA -If SWD -AutoConnect 1 -Speed 50000'"
+            )
+        # Bold green
+        print("\x1B[1m\x1B[32m"
+              f"{type(self).__name__} connected to {self.telnet.host}:{self.telnet.port}."
+              "\x1B[0m")
         return self
 
     def read_blocking(self) -> str:
-        return self.telnet.read_some().decode()
+        """Read any available data and return it as-is."""
+        while True:
+            try:
+                rx_data = self.telnet.read_very_eager()
+            except ConnectionResetError:
+                self.telnet.close()
+                # Bold red
+                return ("\x1B[1m\x1B[31m"
+                        f"{type(self).__name__} disconnected from {self.host}:{self.port}."
+                        "\x1B[0m")
+            if rx_data:
+                break
+            time.sleep(0.01)
+        try:
+            return rx_data.decode('utf-8')
+        except UnicodeDecodeError as e:
+            print(f"While decoding {rx_data}: {e}", file=sys.stderr)
+            return rx_data.decode('utf-8', errors='replace')
 
-    def __next__(self) -> Union[str, Type[StopIteration]]:
-        """Read line, blocking mode"""
-        if not self.connected:
-            return StopIteration
-        # Wait for new line indefinitely, remove newline characters at the end and convert it to string
-        return self.telnet.read_until("\r\n".encode('ascii'))[:-2].decode()
+    def __iter__(self) -> typing.Iterator[str]:
+        """Read line by line and strip all newline characters (\r\n) at the end."""
+        while self.connected:
+            # Wait for new line indefinitely, remove newline characters at the end and convert it to string.
+            # Only wait for \n as some code uses \r\n as newline and other only \n.
+            rx_data = ""
+            while "\n" not in rx_data:
+                rx_data += self.read_blocking()
+            # Multiple lines can be received.
+            for line in rx_data.split("\n"):
+                yield line.strip("\r\n")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.telnet.close()
 
     @property
     def connected(self) -> bool:
-        return self.telnet.get_socket().fileno() != -1
+        return self.telnet.sock and (not self.telnet.eof) and (self.telnet.get_socket().fileno() != -1)
+
+    def __bool__(self) -> bool:
+        return self.connected
 
 
 def main() -> None:
